9ca6043c6e3d25fc14d8c102258868e3a8536022
obi1anzor075 <shindin112@gmail.com>
2025-03-12 23:29:04 +0300

    up

--- AddBinaryImage.sql
+++ AddBinaryImage.sql
@@ -2,6 +2,6 @@ UPDATE DataStoreDB.dbo.Questions
 SET image_data = (
 	SELECT BulkColumn
 	FROM OPENROWSET(BULK 
-	N'C:\Users\Daniil\Documents\GitHub\Quizik_local\PresentationLayer\wwwroot\img\questions\WWII\German_submarin.jpg', SINGLE_BLOB) AS ImageSource
+	N'C:\Users\shind\OneDrive\Документы\GitHub\Quizik_local\PresentationLayer\wwwroot\img\questions\WWII\Torch.jpg', SINGLE_BLOB) AS ImageSource
 )
 WHERE question_id = 51;
